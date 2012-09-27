8727bbc255e7f8b0ab0f813186007d648c6481ff
kdgrocks <mohakraaj@gmail.com>
2012-09-27 14:38:07 +0530

    attributes

--- Preprocess/attributes.py
+++ Preprocess/attributes.py
@@ -1,91 +1,19 @@
-# this file has details about all the Attributes 
+# this file gives crisp numerical values to all the non-numerical values in the data set 
+
+# Attacks categorized to 4 type 
+dos=["back","land","neptune","pod","smurf","teardrop"] #type 1 attack
+r2l=["ftp_write","guess_passwd","imap","multihop","phf","spy","warezclient","warezmaster"] # type 2  
+u2r=["buffer_overflow","loadmodule","perl","rootkit"] #type 3 
+probe=["nmap","satan","ipsweep","portsweep"] # type 4
 
 
-# Numercial values to Protocols are
-tcp=1
-udp=2
-icmp=3
+# List of protocols used 
+Protocols=["tcp","udp","icmp"]
 
-# Numerical values to Services are 
-IRC = 1
-X11 = 2
-Z39_50 = 3
-aol = 4
-auth = 5
-bgp = 6
-courier = 7
-csnet_ns = 8
-ctf = 9
-daytime = 10
-discard = 11
-domain = 12
-domain_u = 13
-echo = 14
-eco_i = 15
-ecr_i = 16
-efs = 17
-finger = 19
-ftp = 20
-ftp_data = 21
-gopher = 22
-harvest = 23
-hostnames = 24
-http = 25
-http_2784 = 26
-http_443 = 27
-http_8001 = 28
-imap4 = 29
-iso_tsap = 30
-klogin = 31
-kshell = 32
-ldap = 33
-link = 34
-login = 35
-mtp = 36
-name = 37
-netbios_dgm = 38
-netbios_ns = 39
-netbios_ssn = 40
-netstat = 41
-nnsp = 42
-nntp = 43
-ntp_u = 44
-other = 45
-pm_dump = 46
-pop_2 = 47
-pop_3 = 48
-printer = 49
-private = 50
-red_i = 51
-remote_job = 52
-rje = 53
-shell = 54
-smtp = 55
-sql_net = 56
-ssh = 57
-sunrpc = 58
-supdup = 59
-systat = 60
-telnet = 61
-tftp_u = 62
-tim_i = 63
-time = 64
-urh_i = 65
-urp_i = 66
-uucp = 67
-uucp_path = 68
-vmnet = 69
-whois = 70
+# List of Services used  
+Services=["IRC","X11","Z39_50","aol","auth","bgp","courier","csnet_ns","ctf","daytime","discard","domain","domain_u","echo ","eco_i ","ecr_i ","efs ","finger","ftp","ftp_data","gopher","harvest","hostnames","http","http_2784","http_443","http_8001","imap4","iso_tsap","klogin","kshell","ldap","link","login","mtp","name","netbios_dgm","netbios_ns","netbios_ssn","netstat","nnsp","nntp","ntp_u","other","pm_dump","pop_2"
+"pop_3","printer","private","red_i","remote_job","rje","shell","smtp","sql_net","ssh","sunrpc","supdup","systat","telnet","tftp_u","tim_i","time","urh_i","urp_i = 66
+"uucp","uucp_path","exec","vmnet","whois"]
 
-# Numerical Values to Flag 
-OTH = 1
-REJ = 2
-RSTO = 3
-RSTOS0 = 4
-RSTR = 5
-S0 = 6
-S1 = 7
-S2 = 8
-S3 = 9
-SF = 10
-SH = 11
+# List of Flags used  
+Flag=["OTH","REJ","RSTO","RSTOS0","RSTR","S0","S1","S2","S3","SF ","SH "]
